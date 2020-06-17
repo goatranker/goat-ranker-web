55c61fb6682f7686423c87129e9a872f43e007e3
Dominic Cobb <dominicshajaa@icloud.com>
2020-06-17 07:53:57 -0700

    front end started

---
+++ src/App/MainNav/index.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react'
+import { Link, useHistory, Redirect } from 'react-router-dom';
+import '../MainNav/style.scss'
+import UserContext from "../context/UserContext.js"
+
+
+export default (props) => {
+    const { userData, setUserData } = useContext(UserContext);
+
+    const history = useHistory()
+
+    const createNew = () => {history.push("/new")}
+    const account = () => {history.push("/users/account")}
+    const signup = () => {history.push("/users/signup")}
+    const login = () => {history.push("/users/login")}
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined
+        })
+        localStorage.setItem('auth-token', '')
+        history.push("/")
+    }
+  
+        return(
+            <div className="nav-cont">
+
+            <nav className="grey darken-3 navbar-fixed">
+            <a href="/">
+                <img className="logo-img" src="" alt="turnout logo"/>
+            </a>
+            
+            <div className="btn-function">
+                {
+                    userData.user? 
+                        <div className="buttons">
+                        <button className="acct-btns" onClick={createNew}>New Event</button>
+                        <button className="acct-btns" onClick={logout}>Logout</button>
+                        <button className="acct-btns" onClick={account}>Account</button>
+                        </div> :
+                    <div className="buttons">
+                    <button className="acct-btns" onClick={login}>Log In</button>
+                    <button className="acct-btns" onClick={signup}>Signup</button>
+                    </div>
+                }
+            </div>
+
+             </nav>
+            </div>
+            
+         )
+}
---
+++ src/App/user/Account.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MainNav from '../MainNav/index.js'
+import { Link } from 'react-router-dom';
+import Moment from 'moment'
+
+export default (props) => {
+
+  
+    return(
+        <div className="account-container">
+          <div className="account-page">
+          <h2 className="account-title">Welcome back.</h2>
+      </div>
+
+     
+        </div>
+        
+    )
+}
