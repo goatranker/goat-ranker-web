ba1380f550155754427d29fd52c242582aa69f78
Dominic Cobb <dominic@Dominics-Mac-mini.local>
2020-06-27 17:58:34 -0700

    asd

--- src/user/Account.js
+++ src/user/Account.js
@@ -1,11 +1,9 @@
 import React, { useContext, createContext} from 'react'
 import { Link, useHistory, Redirect } from 'react-router-dom';
 import UserContext from "../context/UserContext.js"
-import { logDOM } from '@testing-library/react';
 
 export default (props) => {
     const { userData, setUserData } = useContext(UserContext)    
-    console.log(userData);
     
     
     return(
