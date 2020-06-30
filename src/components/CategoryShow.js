748aa94459ec059b25940f01ad775589490742b9
Dominic Cobb <dominic@Dominics-Mac-mini.local>
2020-06-30 00:38:01 -0700

    ranking functional

--- src/components/CategoryShow.js
+++ src/components/CategoryShow.js
@@ -2,24 +2,41 @@ import React,{ useState, useEffect } from "react";
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom';
 import UserContext from "../context/UserContext.js"
-import GoatRanker from '../algorithms/ranker.js'
+// import GoatRanker from '../algorithms/ranker.js'
 import Axios from "axios";
+
+// material 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import StarIcon from '@material-ui/icons/Star';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
 
 const CategoryShow = (props) => {
     const catName = props.location.pathname
     const { userData, setUserData } = useContext(UserContext);
     const [ category, setCategory ] = useState(null)
     const history = useHistory()
+    const classes = useStyles();
+
     console.log('catname',catName);
     console.log('category 2',category);
     
-    const getVotes = async () => {
+    const getRanks = async () => {
         const req = await Axios.get(`http://localhost:8000${catName}`)
-        setCategory(req)
-        
+        setCategory(req.data)
     }
     useEffect(() => {
-        getVotes()
+        getRanks()
     }, [])
 
     return (
@@ -27,16 +44,19 @@ const CategoryShow = (props) => {
         <>
             <br/>
             <br/>
-            <h1>{category.data.name}</h1>
-            <ul>
-            {category.data.userVotes.map((item, index) => {
+            <h1>{category.category.name}</h1>
+
+
+
+            <List component="nav" className={classes.root} aria-label="contacts">
+            {category.ranked.map((item, index) => {
                 return (
-                    <>
-                    <li>{item.artistId}</li>
-                    </>
+                     <ListItem button>
+                    <ListItemText inset primary={item.id}/>
+                    </ListItem>
                 )
             })}
-            </ul>
+            </List>
         </> 
         : 
         <>
