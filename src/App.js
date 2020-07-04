549bae0e43d2e5a756534423c46748ec2d46328e
Dominic Cobb <dominic@Dominics-Mac-mini.local>
2020-07-04 03:26:36 -0700

    idk its pretty late but everything kinda works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom";
 import "./style.scss";
 import UserContext from "./context/UserContext.js";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 
 const App = (props) => {
   const { userData, setUserData } = useContext(UserContext);
@@ -14,13 +16,45 @@ const App = (props) => {
       <br />
       <div className="background">
         <div className="main-cont">
-          {/* <img
-            className="index-img"
-            src={require("./artwork/index_photo.jpeg")}
-            alt="Concert"
-          /> */}
+          <div className="about">
+            <div className="about-1">
+              <h2>How it works</h2>
+              <a href="https://www.dictionary.com/e/slang/g-o-a-t/">
+                What is a GOAT anyway?
+              </a>
+              <p>
+                <br /> Here you can search for your favorite artist just like
+                you would on Spotify! <br />
+                <br />
+                Once you find your favorite artist. Take a look at the
+                categories and see if you think they are worthy of a greatest of
+                all time vote in that genre.
+                <br />
+              </p>
+            </div>
+            <div className="about-2">
+              <p>
+                Most votes in a category will make that artist the GOAT of that
+                genre! <br /> <br /> Goats are displayed in their respective
+                category pages!
+              </p>
+            </div>
+          </div>
+          <div className="spotify-cont">
+            <h4 className="info-title">POWERED BY</h4>
+            <img
+              className="spotify-logo"
+              src={require("./artwork/spotify-logo-png-7072.png")}
+              alt="Spotify Logo"
+            />
+          </div>
+          <a
+            className="home-info-me"
+            href="https://www.github.com/whoisdominic"
+          >
+            Created By Dominic Cobb
+          </a>
         </div>
-        <span>{`(min-width:760px) matches: ${matches}`}</span>
       </div>
     </>
   );
